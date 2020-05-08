629b195039e0e54319a267c8aee05658786d238e
YulkyTulky <ejtracey@optonline.net>
2020-05-07 22:05:47 -0400

    pyzoom for safari

---
+++ pyzoom_safari.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def main():
+    """The user must patch the values in the classrooms dictionary.
+
+    Simply insert a string key representing a subject and make its value
+    a string representing the appropriate zoom link. A template is
+    currently given.
+    """
+
+    classrooms = {
+        "art": "https://zoom.us/j/0000000000",
+        "english": "https://zoom.us/j/0000000000",
+        "history": "https://zoom.us/j/0000000000",
+        "math": "https://zoom.us/j/0000000000",
+        "science": "https://zoom.us/j/0000000000",
+        "language": "https://zoom.us/j/0000000000",
+    }
+
+    room = input("Class: ").lower()
+
+    if room not in classrooms:
+        print("Invalid Classroom.")
+        return
+    else:
+        driver = webdriver.Safari()
+        driver.get(classrooms[room])
+        try:
+            WebDriverWait(driver, 5).until(ec.alert_is_present())
+        except TimeoutException:
+            print("The pop-up alert did not load. Please check your internet connection and try again.")
+        else:
+            driver.switch_to.alert.accept()
+        finally:
+            driver.quit()
+
+
+main()
